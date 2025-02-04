8e323f86f526069885a52288f735f88c15956707
Wasim Khan <wasimshan786@gmail.com>
2025-02-04 12:06:38 +0530

    Practice

--- JavaScript/mockInterview.js
+++ JavaScript/mockInterview.js
@@ -1,48 +1,6 @@
-// TODO: Flatten the Aarray
-const flattenArray = (arr, initialVal = []) => {
-  const newArr = arr.reduce((acc, curr) => {
-    if (Array.isArray(curr)) {
-      flattenArray(curr, acc);
-    } else {
-      acc.push(curr);
-    }
-    return acc;
-  }, initialVal);
-  return newArr;
-};
-
-// console.log(flattenArray([1, 2, [3, 4, [5, 6], 7], 8, [9], [1, 0]]));
-
-// TODO: Implement this | computeAmount().lakh(34).crore(12).lakh(2).thousand(2).lakh(13).value()
-// computeAmount().lakh(34).value()  3400000
-const computeAmount = () => {
-  return {
-    total: 0,
-    thousand(amount) {
-      this.total += amount * 1000;
-      return this;
-    },
-    lakh(amount) {
-      this.total += amount * 100000;
-      return this;
-    },
-    crore(amount) {
-      this.total += amount * 10000000;
-      return this;
-    },
-    value() {
-      return this.total;
-    },
-  };
-};
-
-// console.log(
-//   computeAmount().lakh(34).crore(12).lakh(2).thousand(2).lakh(13).value()
-// );
-
 // TODO: Two way data binding
-const input = document.createElement("input");
-const state = { value: "abc" };
+// const input = document.createElement("input");
+// const state = { value: "abc" };
 
 // const model = (input, state) => {
 //   Object.defineProperty(state);
--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -156,3 +156,45 @@ const memoizeAdd = momoize(add);
 // console.log(memoizeAdd(1, 3));
 // console.log(memoizeAdd(3, 1));
 // console.log(memoizeAdd(2, 13));
+
+// TODO: Flatten the Aarray
+const flattenArray = (arr, initialVal = []) => {
+  const newArr = arr.reduce((acc, curr) => {
+    if (Array.isArray(curr)) {
+      flattenArray(curr, acc);
+    } else {
+      acc.push(curr);
+    }
+    return acc;
+  }, initialVal);
+  return newArr;
+};
+
+// console.log(flattenArray([1, 2, [3, 4, [5, 6], 7], 8, [9], [1, 0]]));
+
+// TODO: Implement this | computeAmount().lakh(34).crore(12).lakh(2).thousand(2).lakh(13).value()
+// computeAmount().lakh(34).value()  3400000
+const computeAmount = () => {
+  return {
+    total: 0,
+    thousand(amount) {
+      this.total += amount * 1000;
+      return this;
+    },
+    lakh(amount) {
+      this.total += amount * 100000;
+      return this;
+    },
+    crore(amount) {
+      this.total += amount * 10000000;
+      return this;
+    },
+    value() {
+      return this.total;
+    },
+  };
+};
+
+// console.log(
+//   computeAmount().lakh(34).crore(12).lakh(2).thousand(2).lakh(13).value()
+// );
